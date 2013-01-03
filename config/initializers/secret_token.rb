4f7c2585dbfff8774c46c7c0845090d5f9a434ae
Andre Onuki dos Santos <otanuki@gmail.com>
2013-01-02 22:24:41 -0200

    Initial commit. Auth using foursquare works, but interface still ugly as hell.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SettleForUs::Application.config.secret_token = '06e1786670cce69d07f1c6dd945f415bcc9309c82b48cc24e68cfb824c857752e21c4ca56b6ba60117c527659dada1c4ef524400fc098b02f037a493b0334305'
